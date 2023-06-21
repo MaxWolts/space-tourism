29c5fe11beb078ef41b8f0781b8c090d2e84df96
Max <max.wolts@gmail.com>
2023-06-21 19:52:24 -0300

    :lipstick: working in the responsive design: little style change in NavDesktop

--- app/components/NavDesktop.tsx
+++ app/components/NavDesktop.tsx
@@ -13,22 +13,22 @@ const NavDesktop = ({ SectionSelectedHanddle, sectionSelected, className }: Prop
         <ul className="flex gap-4 xl:gap-12">
           <li className={"h-24 navText " + `${sectionSelected === 0 && "box-content border-b-2 border-white"}`} >
             <Link href={"/"} onClick={() => SectionSelectedHanddle(0)} className={"flex items-center gap-2 h-full md:text-[1rem] xl:text-[1.125rem]" }>
-              <strong className="invisible xl:visible">00</strong> <span>HOME</span>
+              <strong className="invisible hidden xl:block xl:visible">00</strong> <span>HOME</span>
             </Link>
           </li>
           <li className={"h-24 navText " + `${sectionSelected === 1 && "box-content border-b-2 border-white"}`} >
             <Link href={"/destination/0"} onClick={() => SectionSelectedHanddle(1)} className={"flex items-center gap-2 h-full md:text-[1rem] xl:text-[1.125rem]" }>
-              <strong className="invisible xl:visible">01</strong> <span>DESTINATION</span>
+              <strong className="invisible hidden xl:block xl:visible">01</strong> <span>DESTINATION</span>
             </Link>
           </li>
           <li className={"h-24 navText " + `${sectionSelected === 2 && "box-content border-b-2 border-white"}`} >
             <Link href={"/crew"} onClick={() => SectionSelectedHanddle(2)} className={"flex items-center gap-2 h-full md:text-[1rem] xl:text-[1.125rem]" }>
-              <strong className="invisible xl:visible">02</strong> <span>CREW</span>
+              <strong className="invisible hidden xl:block xl:visible">02</strong> <span>CREW</span>
           </Link>
           </li>
           <li className={"h-24 navText " + `${sectionSelected === 3 && "box-content border-b-2 border-white"}`} >
             <Link href={"/technology"} onClick={() => SectionSelectedHanddle(3)} className={"flex items-center gap-2 h-full md:text-[1rem] xl:text-[1.125rem]" }>
-              <strong className="invisible xl:visible">03</strong> <span>TECHNOLOGY</span>
+              <strong className="invisible hidden xl:block xl:visible">03</strong> <span>TECHNOLOGY</span>
             </Link>
           </li>
         </ul>
