412554a51a1b8685d83b0cf650deb72a5f239819
Max <max.wolts@gmail.com>
2023-06-20 21:42:59 -0300

    :lipstick: ended technology section. mobile first

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { Slide } from "../components/Slide"
 import data from "../data/data.json"
 
-const Crew = () => {
+const Teachnology = () => {
   const [selectedItem, setSelectedItem] = useState<number>(0);
   const handdleSelectedItem = (number:number) => {
     setSelectedItem(number)
@@ -11,20 +11,20 @@ const Crew = () => {
   return (
     <div className="Destination__background-mobile grid gap-8 min-h-screen  pb-6  justify-center text-center">
       <div className="navText pt-[5.5rem]">
-        <p><strong className="text-lightBlue opacity-25">02</strong> MEET YOUR CREW</p>
+        <p><strong className="text-lightBlue opacity-25">03</strong> SPACE LAUNCH 101</p>
       </div>
       <div className="relative">
         <Slide images={data.technology.map(img => img.images.landscape)} version2={true} handdleSelectedItem={handdleSelectedItem} selectedItem={selectedItem}/>
       </div>
       <div className="grid gap-4 px-6">
         <div className="grid gap-2">
-          <span className={"navText font-Bellefair tracking-normal opacity-50"}>{data.crew[selectedItem].role.toUpperCase()}</span>
-          <h4 className="text-[1.5rem]">{data.crew[selectedItem].name.toUpperCase()}</h4>
+          <span className={"navText font-Bellefair tracking-normal opacity-50"}>THE TERMINOLOGY...</span>
+          <h4 className="text-[1.5rem]">{data.technology[selectedItem].name.toUpperCase()}</h4>
         </div>
-        <p className="font-BarlowRegular text-lightBlue leading-6">{data.crew[selectedItem].bio}</p>
+        <p className="font-BarlowRegular text-lightBlue leading-6">{data.technology[selectedItem].description}</p>
       </div>
     </div>
   )
 }
 
-export default Crew
+export default Teachnology
