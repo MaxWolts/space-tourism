fb0632ce9aca1d5a4f84aeac8dfbf7fccee710cf
Max <max.wolts@gmail.com>
2023-06-23 20:44:46 -0300

    :iphone: working in the responsive design: added Destination desktop

--- app/destination/[id]/page.tsx
+++ app/destination/[id]/page.tsx
@@ -9,26 +9,30 @@ const Destinations = ({params}: Props) => {
   const info = data.destinations[id]
   if (id || id === 0) {
     return (
-      <div className="grid justify-center justify-items-center gap-12 mt-16 md:mt-28">
-        <div className="navText md:text-xl md:justify-self-start">
+      <div className="grid justify-center justify-items-center gap-12 mt-16 md:mt-28 xl:mt-[30vh]">
+        <div className="navText md:text-xl md:justify-self-start xl:text-[1.75rem]">
           <p><strong className="text-lightBlue opacity-25 ">01</strong> PICK YOUR DESTINATION</p>
         </div>
-        <Image src={ "/"+info.images.png} className={"h-[18.75rem] w-[18.75rem] "} width={170} height={170} alt={info.name}/>
-        <DestinationNav id={id} />
-        <div className="text-center mb-4">
-          <h3>{info.name}</h3>
-          <p className="regularText font-BarlowRegular text-center text-lightBlue max-w-[35.813rem]">
-            {info.description}
-          </p>
-        </div>
-        <div className="grid gap-5 pb-5 pt-8 border-t border-[#383B4B] w-full text-center md:grid-cols-2">
-          <div className="grid gap-3">
-            <p className="subHeading2 text-lightBlue">AVG. DISTANCE</p>
-            <p className="subHeading1">{info.distance.toUpperCase()}</p>
-          </div>
-          <div className="grid gap-3">
-            <p className="subHeading2 text-lightBlue">EST. TRAVEL TIME</p>
-            <p className="subHeading1">{info.travel.toUpperCase()}</p>
+        <div className="grid justify-items-center gap-7 xl:grid-cols-[minmax(27.813rem,_1fr)_minmax(27.813rem,_1fr)] xl:gap-[9.813rem]">
+          <Image src={ "/"+info.images.png} className={"h-[18.75rem] w-[18.75rem] xl:w-[27.813rem] xl:h-[27.813rem]"} width={170} height={170} alt={info.name}/>
+          <div className="grid justify-items-center gap-7">
+            <DestinationNav id={id} />
+            <div className="text-center mb-4">
+              <h3>{info.name}</h3>
+              <p className="regularText font-BarlowRegular text-center text-lightBlue max-w-[35.813rem] xl:max-w-[27.813rem] xl:text-left">
+                {info.description}
+              </p>
+            </div>
+            <div className="grid gap-5 pb-5 pt-8 border-t border-[#383B4B] w-full text-center md:grid-cols-2">
+              <div className="grid gap-3">
+                <p className="subHeading2 text-lightBlue">AVG. DISTANCE</p>
+                <p className="subHeading1">{info.distance.toUpperCase()}</p>
+              </div>
+              <div className="grid gap-3">
+                <p className="subHeading2 text-lightBlue">EST. TRAVEL TIME</p>
+                <p className="subHeading1">{info.travel.toUpperCase()}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
