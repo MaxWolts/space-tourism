cf5aa57ff7a3100a15d50bfcf52800b2ea4efe38
Max <max.wolts@gmail.com>
2023-06-28 01:26:47 -0300

    :lipstick: changes in the Home desktop

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <main className={`z-10 relative w-full min-h-screen Home__background bg-cover `}>
       <Header itemSelected={0}/>
-      <div className="pt-14 pb-16 md:pb-20 px-6 text-center md:pt-[6.625rem] md:px-0 md:mx-auto xl:flex xl:items-end xl:m-0 xl:text-left xl:pb-[16vh] xl:max-w-7xl xl:mx-auto xl:h-fit xl:pt-64">
-        <div className="max-w-md mx-auto">
+      <div className="pt-14 pb-16 md:pb-20 px-6 text-center md:pt-[6.625rem] md:px-0 md:mx-auto xl:flex xl:items-end xl:m-0 xl:text-left xl:pb-[16vh] xl:max-w-full xl:h-fit xl:pt-64 xl:justify-around">
+        <div className="max-w-md mx-auto xl:m-0">
           <div className="navText text-lightBlue">
             <p className="md:text-[1.25rem] xl:text-[1.75rem] xl:tracking-[0.295rem]">SO, YOU WANT TO TRAVEL TO</p>
           </div>
@@ -19,7 +19,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="relative w-fit mx-auto mt-20">
+        <div className="relative w-fit mx-auto mt-20 xl:m-0">
           <Link href={"/destination/0"} className={" z-10 explore relative grid content-center text-center bg-white rounded-full h-[9.375rem] w-[9.375rem] md:h-[15.125rem] md:w-[15.125rem] xl:w-[17.125] xl:h-[17.125]"}>
             <p className="text-[1.25rem] font-Bellefair tracking-[1.25px] md:text-[2rem]">EXPLORE</p>
           </Link>
